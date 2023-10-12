101eb4ab23bfacb88f078d88b6e5aa3e8c902d0e
JasurbekErgashev <jergashev633@gmail.com>
2023-10-12 21:24:10 +0500

    added plugic source code

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <network_info_plus_windows/network_info_plus_windows_plugin.h>
+#include <smart_bluetooth_pos_printer/flutter_pos_printer_platform_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
+  FlutterPosPrinterPlatformPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterPosPrinterPlatformPlugin"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  network_info_plus_windows
+  smart_bluetooth_pos_printer
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
