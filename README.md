7e44f87b78ce3872d4a3bbe3582c970c52f7ebc2
JasurbekErgashev <jergashev633@gmail.com>
2023-10-12 21:06:11 +0500

    initial template

---
+++ README.md
@@ -0,0 +1,4 @@
+# smart_bluetooth_pos_printer_testapp
+
+A new Flutter project.
+
---
+++ android/app/src/main/kotlin/uz/smarts/smart_bluetooth_pos_printer_testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package uz.smarts.smart_bluetooth_pos_printer_testapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
